4c95b586159bbc2b0540233998fc6bf2cc5a0ca9
Kyle Ong <ongkyle@live.com.my>
2021-04-09 16:25:39 +0800

    Delete starreco/dataset
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/data/dataset/__init__.py
+++
@@ -1,5 +0,0 @@
-from starreco.data.dataset.dataframe import User, Item, Rating
-from starreco.data.dataset.dataset import Dataset
-from starreco.data.dataset.movielens_dataset import MovielensDataset
-from starreco.data.dataset.epinions_dataset import EpinionsDataset
-from starreco.data.dataset.book_crossing_dataset import BookCrossingDataset
