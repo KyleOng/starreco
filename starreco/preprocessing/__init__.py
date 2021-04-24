4e6c688232480a582ea28fb2b840bd5809e46b2f
Kyle Ong <ongkyle@live.com.my>
2021-04-24 18:27:47 +0800

    FIX: update imports
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/preprocessing/__init__.py
+++ starreco/preprocessing/__init__.py
@@ -1,2 +1,1 @@
-from starreco.preprocessing.preprocessor import Preprocessor
-from starreco.preprocessing.utils import sparse_coo_to_tensor, ratings_to_sparse
+from .preprocessor import Preprocessor
