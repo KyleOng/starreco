44a5555c93ccb53b6c139001f25415260d1ec807
Kyle Ong <ongkyle@live.com.my>
2021-04-26 19:23:47 +0800

    FIX: added model attribute to module
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/model/mf.py
+++ starreco/model/mf.py
@@ -8,37 +8,41 @@ class MF(torch.nn.Module):
     """
     Matrix Factorization
     """
+
     def __init__(self, field_dims:list, 
                  embed_dim:int = 8):
         super().__init__()
 
         # Embedding layer
-        self.embedding = FeaturesEmbedding(field_dims, embed_dim)
+        self.features_embedding = FeaturesEmbedding(field_dims, embed_dim)
 
     def forward(self, x):
         # Generate embeddings
-        x_embed = self.embedding(x)
+        x_embed = self.features_embedding(x.int())
         # Seperate user (1st column) and item (2nd column) embeddings from generated embeddings
         user_embed = x_embed[:, 0]
         item_embed = x_embed[:, 1]
 
         # Dot product between user and items embeddings
-        dot = torch.mm(user_embed, item_embed.T)
-        # Obtain diagonal part of the dot product result
-        diagonal = dot.diag()
-        # Reshape to match output shape
-        y = diagonal.view(diagonal.shape[0], -1)
+        dot = torch.sum(user_embed * item_embed, dim = 1)
+        
+        # Reshape to match target shape
+        y = dot.view(dot.shape[0], -1)
 
         return y
 
 class MFModule(BaseModule):
+    """
+    Matrix Factorization Module
+    """
+
     def __init__(self, field_dims:list, 
                  embed_dim:int = 8, 
                  lr:float = 1e-3,
                  weight_decay:float = 1e-6,
                  criterion:F = F.mse_loss):
-        super().__init__(lr, weight_decay, criterion)
-        self.model = MF(field_dims, embed_dim)
+        model = MF(field_dims, embed_dim)
+        super().__init__(model, lr, weight_decay, criterion)
         self.save_hyperparameters()
     
 
