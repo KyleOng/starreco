4487310ac9f219a16d907e08320d7f5b588d10a8
Kyle Ong <ongkyle@live.com.my>
2021-04-28 13:14:42 +0800

    FEAT: Add NCF model
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/model/__init__.py
+++ starreco/model/__init__.py
@@ -1,3 +1,4 @@
 from .mf import MF
 from .gmf import GMF
+from .ncf import NCF
 from .mdacf import MDACF
--- starreco/model/ncf.py
+++ starreco/model/ncf.py
@@ -7,43 +7,12 @@ import torch.nn.functional as F
 from .module import BaseModule
 from .layer import FeaturesEmbedding, MultilayerPerceptrons
 
-class NCF(torch.nn.Module):
+class NCF(BaseModule):
     """
     Neural Collaborative Filtering
     """
+
     def __init__(self, field_dims:list, 
-                 embed_dim:int = 8,
-                 hidden_dims:list = [32, 16, 8], 
-                 activations:Union[str, list] = "relu", 
-                 dropouts:Union[float, list] = 0.5, 
-                 batch_norm:bool = True):
-        super().__init__()
-
-        # Embedding layer
-        self.embedding = FeaturesEmbedding(field_dims, embed_dim)
-
-        # Multilayer perceptrons
-        self.mlp = MultilayerPerceptrons(input_dim = embed_dim * 2, 
-                                         hidden_dims = hidden_dims, 
-                                         activations = activations, 
-                                         dropouts = dropouts,
-                                         output_layer = "relu",
-                                         batch_norm = batch_norm)
-
-    def forward(self, x):
-        # Generate embeddings
-        x_embed = self.embedding(x)
-
-        # Concatenate embeddings
-        x_embed = torch.flatten(x_embed, start_dim = 1)
-
-        # Prediction
-        y = self.mlp(x_embed)
-
-        return y
-
-class NCFModule(BaseModule):
-     def __init__(self, field_dims:list, 
                  embed_dim:int = 8,
                  hidden_dims:list = [32, 16, 8], 
                  activations:Union[str, list] = "relu", 
@@ -54,7 +23,28 @@ class NCFModule(BaseModule):
                  criterion:F = F.mse_loss,
                  save_hyperparameters:bool = True):
         super().__init__(lr, weight_decay, criterion)
-        self.model = NCF(field_dims, embed_dim, hidden_dims, activations, dropouts, batch_norm)
+
+        # Embedding layer
+        self.features_embedding = FeaturesEmbedding(field_dims, embed_dim)
+
+        # Network 
+        self.net = MultilayerPerceptrons(input_dim = embed_dim * 2, 
+                                         hidden_dims = hidden_dims, 
+                                         activations = activations, 
+                                         dropouts = dropouts,
+                                         output_layer = "relu",
+                                         batch_norm = batch_norm)
+
         self.save_hyperparameters()
-    
+
+    def forward(self, x):
+        # Generate embeddings
+        x_embed = self.features_embedding(x.int())
+
+        # Concatenate embeddings
+        concat = torch.flatten(x_embed, start_dim = 1)
         
+        # Feed concatenate embeddings to network
+        y = self.net(concat)
+
+        return y
