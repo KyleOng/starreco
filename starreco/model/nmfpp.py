75016e0f1ad44c1e4afcb4cf71c2d275154c7076
Kyle Ong <ongkyle@live.com.my>
2021-05-01 16:11:01 +0800

    FIX: Rewrite NMFPP
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/model/nmfpp.py
+++ starreco/model/nmfpp.py
@@ -6,92 +6,97 @@ import torch.nn.functional as F
 
 from .module import BaseModule
 from .layer import FeaturesEmbedding, MultilayerPerceptrons
-from .gmfpp import GMFPP
-from .ncfpp import NCFPP
+from .gmfpp import GMFPPmodel
+from .ncfpp import NCFPPmodel
 
-class NMFPP(torch.nn.Module):
+class NMFPPmodel(torch.nn.Module):
     """
-    Neural Matrix Factorizatgion ++
+    Neural Matrix Factorizatgion ++ model
     """
-    def __init__(self, gmfpp:GMFPP, ncfpp:NCFPP,
-                 pretrain:bool = False):
+    def __init__(self, 
+                 gmfpp:GMFPPmodel, 
+                 ncfpp:NCFPPmodel,
+                 freeze_pretrain:bool):
         super().__init__()
+
+        # Clone pretrained models
         self.gmfpp = copy.deepcopy(gmfpp)
         self.ncfpp = copy.deepcopy(ncfpp)
 
-        # Freeze embedding weights if pretrain
-        if pretrain:
-            self.gmfpp.embedding.embedding.weight.requires_grad = False
-            self.ncfpp.embedding.embedding.weight.requires_grad = False
+        # Freeze model
+        if freeze_pretrain:
+            for param in self.gmfpp.parameters():
+                param.requires_grad = False
+            for param in self.ncfpp.parameters():
+                param.requires_grad = False
 
-        # Remove GMFPP output layer
-        self.gmfpp.slp = None
+        # Remove GMF output layer
+        del self.gmfpp.net
 
-        # Add GMF embedding dim to `input_dim`
-        input_dim = self.gmfpp.embedding.embedding.weight.shape[1]
+        # Remove NCF output layer
+        del self.ncfpp.net.mlp[-1]
+        if type(self.ncfpp.net.mlp[-1]) == torch.nn.Linear:
+            del self.ncfpp.net.mlp[-1]
 
-        # Remove NCFPP output layer and freeze hidden weights and biases
-        ncfpp_mlp_blocks = []
-        for i, module in enumerate(self.ncfpp.mlp.mlp):
-            # Freeze weights and bias if pretrain
-            if hasattr(self.ncfpp.mlp.mlp[i], "weight") and pretrain:
-                self.ncfpp.mlp.mlp[i].weight.requires_grad = False
-            if hasattr(self.ncfpp.mlp.mlp[i], "bias") and pretrain:
-                self.ncfpp.mlp.mlp[i].bias.requires_grad = False
-            if type(module) == torch.nn.Linear:
-                if module.out_features == 1:
-                    # Add NCFPP last hidden input dim to `input_dim`
-                    input_dim += module.in_features 
-                    break
-            ncfpp_mlp_blocks.append(module)
-        self.ncfpp.mlp = torch.nn.Sequential(*ncfpp_mlp_blocks)
-        
-        # Singlelayer perceptrons
-        # input_dim = GMF embedding dim + ncfpp last hidden input dim
-        self.slp = MultilayerPerceptrons(input_dim = input_dim, 
+        # Get input dim 
+        input_dim = self.gmfpp.features_embedding.embedding.embedding_dim 
+        input_dim += self.gmfpp.user_ae.decoder.mlp[0].in_features and self.gmfpp.item_ae.decoder.mlp[0].in_features 
+        for i in range(0, len(self.ncfpp.net.mlp))[::-1]:
+            if type(self.ncfpp.net.mlp[i]) == torch.nn.Linear:
+                input_dim += self.ncfpp.net.mlp[i].out_features
+                break
+        # Hybrid network
+        self.net = MultilayerPerceptrons(input_dim = input_dim, 
                                          output_layer = "relu")
 
-    def forward(self, x):
-        # GMF part
-        gmf_x_embed = self.gmf.embedding(x)
-        gmf_user_embed = gmf_x_embed[:, 0]
-        gmf_item_embed = gmf_x_embed[:, 1]
-        # Element wise product between embeddings
-        gmf_product = gmf_user_embed *  gmf_item_embed
+    def forward(self, x, user_x, item_x):
+        # GMF part: Element wise product between embeddings
+        gmf_product = self.gmfpp.encode_element_wise_product(x, user_x, item_x)
 
-        # NCF part
-        # Get output of last hidden layer
-        ncf_last_hidden = self.ncf(x)
+        # NCF part: Get output of last hidden layer
+        ncf_last_hidden = self.ncfpp.forward(x, user_x, item_x)
 
         # Concatenate GMF's element wise product and NCF's last hidden layer output
         concat = torch.cat([gmf_product, ncf_last_hidden], dim = 1)
 
         # Feed to generalized non-linear layer
-        y = self.slp(concat)
+        y = self.net(concat)
 
         return y
 
-class NMFPPModule(BaseModule):
-    def __init__(self, gmfpp:GMFPP, ncfpp:NCFPP,
-                 pretrain:bool = False,
+
+class NMFPP(BaseModule):
+    def __init__(self, 
+                 gmfpp:GMFPPmodel, 
+                 ncfpp:NCFPPmodel,
+                 freeze_pretrain:bool = True,
+                 alpha:Union[int,float] = 0.1, 
+                 beta:Union[int,float] = 0.1,
+                 gamma:Union[int,float] = 0.1, 
+                 delta:Union[int,float] = 0.1,
                  lr:float = 1e-3,
                  weight_decay:float = 1e-6,
                  criterion:F = F.mse_loss,
                  save_hyperparameters:bool = True):
         super().__init__(lr, weight_decay, criterion)
-        self.model = NMFPP(gmfpp, ncfpp, pretrain)
+        self.model = NMFPPmodel(gmfpp, ncfpp, freeze_pretrain)
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        self.delta = delta
         self.save_hyperparameters()
+    
+    def forward(self, *batch):
+        return self.model.forward(*batch)
 
-    def evaluate(self, x, y): 
-        # Partition user and item features
-        user_feature = x[:, 2:self.model.user_feature_dim + 2]
-        item_feature = x[:, -self.model.item_feature_dim:]
+    def backward_loss(self, *batch):
+        x, user_x, item_x, y = batch
 
         loss = 0
-        loss += self.criterion(self.model.gmfpp.user_ae.forward(user_feature), user_feature)
-        loss += self.criterion(self.model.gmfpp.item_ae.forward(item_feature), item_feature)
-        loss += self.criterion(self.model.ncfpp.user_ae.forward(user_feature), user_feature)
-        loss += self.criterion(self.model.ncfpp.item_ae.forward(item_feature), item_feature)
-        loss += super().evaluate(x, y)
+        loss += self.alpha * self.criterion(self.model.gmfpp.user_ae.forward(user_x), user_x)
+        loss += self.beta * self.criterion(self.model.gmfpp.item_ae.forward(item_x), item_x)
+        loss += self.gamma * self.criterion(self.model.ncfpp.user_ae.forward(user_x), user_x)
+        loss += self.delta * self.criterion(self.model.ncfpp.item_ae.forward(item_x), item_x)
+        loss += super().backward_loss(*batch)
 
         return loss
