b95f98c4e14afae6a9568d0c5739d1239f415798
Kyle Ong <ongkyle@live.com.my>
2021-05-01 16:09:29 +0800

    FIX: Rewrite GMF
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/model/gmf.py
+++ starreco/model/gmf.py
@@ -7,17 +7,14 @@ import torch.nn.functional as F
 from .module import BaseModule
 from .layer import FeaturesEmbedding, MultilayerPerceptrons
 
-class GMF(BaseModule):
+class GMFmodel(torch.nn.Module):
     """
-    Generalized Matrix Factorization
+    Generalized Matrix Factorization model
     """
 
     def __init__(self, field_dims:list, 
-                 embed_dim:int = 8, 
-                 lr:float = 1e-3,
-                 weight_decay:float = 1e-6,
-                 criterion:F = F.mse_loss):
-        super().__init__(lr, weight_decay, criterion)
+                 embed_dim:int):
+        super().__init__()
 
         # Embedding layer
         self.features_embedding = FeaturesEmbedding(field_dims, embed_dim)
@@ -26,21 +23,39 @@ class GMF(BaseModule):
         self.net = MultilayerPerceptrons(input_dim = embed_dim, 
                                          output_layer = "relu")
 
-        self.save_hyperparameters()
-
-    def forward(self, x):
+    def element_wise_product(self, x):
         # Generate embeddings
         x_embed = self.features_embedding(x.int())
+
         # Seperate user (1st column) and item (2nd column) embeddings from generated embeddings
         user_embed = x_embed[:, 0]
         item_embed = x_embed[:, 1]
 
+        return user_embed * item_embed
+
+    def forward(self, x):
         # Element wise product between user and items embeddings
-        product = user_embed * item_embed
+        product = self.element_wise_product(x)
         
         # Feed element wise product to generalized non-linear layer
         y = self.net(product)
 
         return y
     
+class GMF(BaseModule):
+    """
+    Generalized Matrix Factorization lightning module
+    """
+
+    def __init__(self, field_dims:list, 
+                 embed_dim:int = 8, 
+                 lr:float = 1e-3,
+                 weight_decay:float = 1e-6,
+                 criterion:F = F.mse_loss):
+        super().__init__(lr, weight_decay, criterion)
+        self.model = GMFmodel(field_dims, embed_dim)
+        self.save_hyperparameters()
+
+    def forward(self, *batch):
+        return self.model.forward(*batch)
         
