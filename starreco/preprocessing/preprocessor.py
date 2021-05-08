dedd7fc7f8d93a72dde945a8838a174d4a590d5c
Kyle Ong <ongkyle@live.com.my>
2021-05-08 10:44:31 +0800

    FIX: move CustomMultiLabelBinarizer from preprocessor.py to custom.py
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/preprocessing/preprocessor.py
+++ starreco/preprocessing/preprocessor.py
@@ -6,29 +6,9 @@ from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler, MultiLabelBinarizer, OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 
-class CustomMultiLabelBinarizer(MultiLabelBinarizer):
-    """
-    Custom MultiLabelBinarizer.
-
-    Notes: Original MultiLabelBinarizer fit_transform() only takes 2 positional arguments. However, our custom pipeline assumes the MultiLabelBinarizer fit_transform() is defined to take 3 positional arguments. Hence, adding an additional argument y to fit_transform() fix the problem.
-    """
-
-    def fit_transform(self, X, y = None):
-        """
-        Fix original MultiLabelBinarizer fit_transform().
-
-        :param X: X.
-
-        :param y: y (target), set as None.
-
-        :return: transformed X.      
-        """
-        y = None
-        if not isinstance(X, np.ndarray):
-            X = np.array(X, dtype = "object")
-        return super().fit_transform(X.flatten())
+from .custom import CustomMultiLabelBinarizer
 
 
 class Preprocessor:
