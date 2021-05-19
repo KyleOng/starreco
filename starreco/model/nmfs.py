9922a37319d7b90257079fcf1fcd7f6d8a9332f1
Kyle Ong <ongkyle@live.com.my>
2021-05-19 11:21:24 +0800

    FIX: freeze_pretrain use super loss function
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/model/nmfs.py
+++ starreco/model/nmfs.py
@@ -21,11 +21,15 @@ class NMFS(BaseModule):
                  ncfpp_params:dict = None,
                  freeze_pretrain:bool = True,
                  lr:float = 1e-3,
-                 weight_decay:float = 1e-6,
+                 l2_lambda:float = 1e-3,
                  criterion:F = F.mse_loss):
-        super().__init__(lr, weight_decay, criterion)
+        if freeze_pretrain:
+            super().__init__(lr, l2_lambda, criterion)
+        else:
+            super().__init__(lr, 0, criterion)
+            self.l2_lambda = l2_lambda
         self.save_hyperparameters(ignore = ["gmfpp_params", "ncfpp_params"])
-        
+
         self.gmfpp = GMFPP(**gmfpp_hparams)
         self.ncfpp = NCFPP(**ncfpp_hparams)
 
@@ -54,6 +58,7 @@ class NMFS(BaseModule):
         input_dim += ncfpp_hparams["ncf_hparams"]["hidden_dims"][-1]
         self.net = MultilayerPerceptrons(input_dim = input_dim, 
                                          output_layer = "relu")
+        self.freeze_pretrain = freeze_pretrain
 
     def forward(self, x, user, item):
         # GMFPP part: Element wise product between latent factor and embeddings
@@ -71,16 +76,48 @@ class NMFS(BaseModule):
         return y
 
     def backward_loss(self, *batch):
-        x, user_x, item_x, y = batch
+        if self.freeze_pretrain:
+            return super().backward_loss(*batch)
+        else:
+            x, user_x, item_x, y = batch
 
-        loss = 0
-        loss += self.gmfpp.alpha * self.criterion(self.gmfpp.user_ae.forward(user_x), user_x)
-        loss += self.gmfpp.beta  * self.criterion(self.gmfpp.item_ae.forward(item_x), item_x)
-        loss += self.ncfpp.alpha * self.criterion(self.ncfpp.user_ae.forward(user_x), user_x)
-        loss += self.ncfpp.beta  * self.criterion(self.ncfpp.item_ae.forward(item_x), item_x)
-        loss += super().backward_loss(*batch)
+            gmfpp_user_loss = self.criterion(self.gmfpp.user_ae.forward(user_x), user_x)
+            gmfpp_user_l2_reg = torch.tensor(0.).to(self.device)
+            for param in self.gmfpp.user_ae.parameters():
+                gmfpp_user_l2_reg += torch.norm(param).to(self.device)
+            gmfpp_user_loss += self.l2_lambda * gmfpp_user_l2_reg
 
-        return loss
+            gmfpp_item_loss = self.criterion(self.gmfpp.item_ae.forward(item_x), item_x)
+            gmfpp_item_l2_reg = torch.tensor(0.).to(self.device)
+            for param in self.gmfpp.item_ae.parameters():
+                gmfpp_item_l2_reg += torch.norm(param).to(self.device)
+            gmfpp_item_loss += self.l2_lambda * gmfpp_item_l2_reg
+
+            ncfpp_user_loss = self.criterion(self.ncfpp.user_ae.forward(user_x), user_x)
+            ncfpp_user_l2_reg = torch.tensor(0.).to(self.device)
+            for param in self.ncfpp.user_ae.parameters():
+                ncfpp_user_l2_reg += torch.norm(param).to(self.device)
+            ncfpp_user_loss += self.l2_lambda * ncfpp_user_l2_reg
+
+            ncfpp_item_loss = self.criterion(self.ncfpp.item_ae.forward(item_x), item_x)
+            ncfpp_item_l2_reg = torch.tensor(0.).to(self.device)
+            for param in self.ncfpp.item_ae.parameters():
+                ncfpp_item_l2_reg += torch.norm(param).to(self.device)
+            ncfpp_item_loss += self.l2_lambda * ncfpp_item_l2_reg
+
+            cf_loss = super().backward_loss(*batch)
+            cf_l2_reg = torch.tensor(0.).to(self.device)
+            for param in self.gmfpp.gmf.parameters():
+                cf_l2_reg += torch.norm(param).to(self.device)
+            for param in self.ncfpp.ncf.parameters():
+                cf_l2_reg += torch.norm(param).to(self.device)
+            for param in self.net.parameters():
+                cf_l2_reg += torch.norm(param).to(self.device)
+            cf_loss += self.l2_lambda * cf_l2_reg
+
+            loss = cf_loss + self.gmfpp.alpha * gmfpp_user_loss + self.gmfpp.beta * gmfpp_item_loss + self.ncfpp.alpha * ncfpp_user_loss + self.ncfpp.beta  * ncfpp_item_loss
+
+            return loss
 
 
 
