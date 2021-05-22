b661d7c497aaacba9d17edca48e83af251fae25c
Kyle Ong <ongkyle@live.com.my>
2021-05-22 21:08:01 +0800

    STYLE: comment
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/model/utils.py
+++ starreco/model/utils.py
@@ -1,20 +1,24 @@
 import torch
 
+# Done
 def weight_init(m):
-    # Weight initialization on specific layer
+    """Weight initialization on specific layer."""
     if type(m) == torch.nn.Linear:
         torch.nn.init.xavier_uniform_(m.weight)
     
         if m.bias is not None:
             torch.nn.init.zeros_(m.bias)
 
+# Done
 def freeze_partial_linear_params(layer, weight_indices, bias_indices = None, dim = 0):
+    """Freeze partial weights on a linear layer."""
     def freezing_hook_weight_full(grad, weight_multiplier):
         return grad * weight_multiplier.to(grad.device)
 
     def freezing_hook_bias_full(grad, bias_multiplier):
         return grad * bias_multiplier.to(grad.device)
 
+    # Map which weights to be freezed
     weight_multiplier = torch.ones(layer.weight.shape).to(layer.weight.device)
     bias_multiplier = torch.ones(layer.bias.shape).to(layer.bias.device)
     if dim:
@@ -25,6 +29,7 @@ def freeze_partial_linear_params(layer, weight_indices, bias_indices = None, dim
     if bias_indices:
         bias_multiplier[bias_indices] = 0
 
+    # Register hook
     freezing_hook_weight = lambda grad: freezing_hook_weight_full(grad, weight_multiplier)
     freezing_hook_bias = lambda grad: freezing_hook_bias_full(grad, bias_multiplier)
 
