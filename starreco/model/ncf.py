d7dbf739bc7fd074d193a63ae22c3607897d2286
Kyle Ong <ongkyle@live.com.my>
2021-05-24 09:37:16 +0800

    FIX: remove argument save_hyperparamters
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/model/ncf.py
+++ starreco/model/ncf.py
@@ -10,7 +10,17 @@ from .layer import FeaturesEmbedding, MultilayerPerceptrons
 # Done
 class NCF(BaseModule):
     """
-    Neural Collaborative Filtering lightning module
+    Neural Collaborative Filtering.
+
+    - field_dims (list): List of features dimensions.
+    - embed_dim (int): Embedding dimension.
+    - hidden_dims (list): List of numbers of neurons throughout the hidden layers. Default: [32,16,8].
+    - activations (str/list): List of activation functions. Default: relu.
+    - dropouts (int/float/list): List of dropout values. Default: 0.5.
+    - batch_norm (bool): If True, apply batch normalization in every layer. Batch normalization is applied between activation and dropout layer. Default: True.
+    - lr (float): Learning rate.
+    - l2_lambda (float): L2 regularization rate.
+    - criterion (F): Criterion or objective or loss function.
     """
 
     def __init__(self, field_dims:list, 
@@ -21,8 +31,7 @@ class NCF(BaseModule):
                  batch_norm:bool = True,
                  lr:float = 1e-3,
                  l2_lambda:float = 1e-3,
-                 criterion:F = F.mse_loss,
-                 save_hyperparameters:bool = True):
+                 criterion:F = F.mse_loss):
         super().__init__(lr, l2_lambda, criterion)
         self.save_hyperparameters()
         
