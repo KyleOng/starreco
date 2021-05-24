615967eded2922f9619529b41c2a640ee0497d4c
Kyle Ong <ongkyle@live.com.my>
2021-05-24 11:33:57 +0800

    FIX: Done
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/model/gmfpp.py
+++ starreco/model/gmfpp.py
@@ -4,14 +4,28 @@ import torch
 import torch.nn.functional as F
 
 from .module import BaseModule
-from .sdae import SDAE
+from .layer import StackedDenoisingAutoEncoder
 from .gmf import GMF
 
-
+# Done
 class GMFPP(BaseModule):
+    """
+    Generalized Matrix Factorization ++.
+
+    - user_sdae_hparams (dict): User SDAE hyperparameteres.
+    - item_sdae_hparams (dict): Item SDAE hyperparameteres.
+    - gmf_hparams (dict): GMF hyperparameteres.
+    - gmf_params (dict): GMF pretrain weights/parameteers.
+    - alpha (int/float): Trade-off parameter value for user SDAE.
+    - beta (int/float): Trade-off parameter value for item SDAE.
+    - lr (float): Learning rate.
+    - l2_lambda (float): L2 regularization rate.
+    - criterion (F): Criterion or objective or loss function.
+    """
+
     def __init__(self, 
-                 user_ae_hparams:dict, 
-                 item_ae_hparams:dict, 
+                 user_sdae_hparams:dict, 
+                 item_sdae_hparams:dict, 
                  gmf_hparams:dict,
                  gmf_params:dict = None,
                  alpha:Union[int,float] = 1, 
@@ -19,7 +33,7 @@ class GMFPP(BaseModule):
                  lr:float = 1e-3,
                  l2_lambda:float = 1e-3,
                  criterion:F = F.mse_loss):
-        assert user_ae_hparams["hidden_dims"][-1] and item_ae_hparams["hidden_dims"][-1],\
+        assert user_sdae_hparams["hidden_dims"][-1] and item_sdae_hparams["hidden_dims"][-1],\
         "user SDAE and item SDAE latent dimension must be the same"
 
         super().__init__(lr, 0, criterion)
@@ -29,15 +43,13 @@ class GMFPP(BaseModule):
         self.alpha = alpha
         self.beta = beta
 
-        self.user_ae = SDAE(**user_ae_hparams)
-        self.item_ae = SDAE(**item_ae_hparams)
+        self.user_sdae = StackedDenoisingAutoEncoder(**user_sdae_hparams)
+        self.item_sdae = StackedDenoisingAutoEncoder(**item_sdae_hparams)
         self.gmf = GMF(**gmf_hparams)
-
-        if gmf_params:
-            self.gmf.load_state_dict(gmf_params)
+        if gmf_params: self.gmf.load_state_dict(gmf_params)
 
         # Replace the first layer with reshape input features
-        latent_dim = user_ae_hparams["hidden_dims"][-1] and item_ae_hparams["hidden_dims"][-1]
+        latent_dim = user_sdae_hparams["hidden_dims"][-1] and item_sdae_hparams["hidden_dims"][-1]
         input_dim = self.gmf.net.mlp[0].in_features
         output_dim = self.gmf.net.mlp[0].out_features
         # Obtain first layer weights before reshaping
@@ -50,8 +62,8 @@ class GMFPP(BaseModule):
 
     def encode_element_wise_product(self, x, user_x, item_x):
         # Obtain latent factor z
-        user_z = self.user_ae.encode(user_x)
-        item_z = self.item_ae.encode(item_x)
+        user_z = self.user_sdae.encode(user_x)
+        item_z = self.item_sdae.encode(item_x)
 
         # Obtain product of latent factor and embeddings        
         z_product = user_z * item_z
@@ -72,17 +84,18 @@ class GMFPP(BaseModule):
         return y
 
     def backward_loss(self, *batch):
+        """Custom backward loss"""
         x, user_x, item_x, y = batch
 
-        user_loss = self.criterion(self.user_ae.forward(user_x), user_x)
+        user_loss = self.criterion(self.user_sdae.forward(user_x), user_x)
         user_l2_reg = torch.tensor(0.).to(self.device)
-        for param in self.user_ae.parameters():
+        for param in self.user_sdae.parameters():
             user_l2_reg += torch.norm(param).to(self.device)
         user_loss += self.l2_lambda * user_l2_reg
 
-        item_loss = self.criterion(self.item_ae.forward(item_x), item_x)
+        item_loss = self.criterion(self.item_sdae.forward(item_x), item_x)
         item_l2_reg = torch.tensor(0.).to(self.device)
-        for param in self.item_ae.parameters():
+        for param in self.item_sdae.parameters():
             item_l2_reg += torch.norm(param).to(self.device)
         item_loss += self.l2_lambda * item_l2_reg
 
