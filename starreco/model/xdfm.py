0a1a8a649e3a1084299f4859a1b5c153c17bece5
Kyle Ong <ongkyle@live.com.my>
2021-05-25 16:26:13 +0800

    FEAT: add XDFM
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

---
+++ starreco/model/xdfm.py
@@ -0,0 +1,70 @@
+from starreco.model.module import BaseModule
+from typing import Union
+
+import torch
+import torch.nn.functional as F
+
+from .module import BaseModule
+from .layer import FeaturesEmbedding, FeaturesLinear, CompressedInteraction, MultilayerPerceptrons
+
+# Done
+class XDFM(BaseModule):
+    """
+    Neural Factorization Machine.
+
+    - field_dims (list): List of features dimensions.
+    - embed_dim (int): Embedding dimension. Default: 10.
+    - hidden_dims (list): List of numbers of neurons across the hidden layers. Default: [400, 400, 400].
+    - activations (str/list): List of activation functions. Default: relu.
+    - dropouts (int/float/list): List of dropout values. Default: 0.5.
+    - cross_dims: List of numbers of neurons across cross interaction layers. Default: [200, 200]
+    - cross_split_half: If True, convolution output across the cross interaction layers is splitted into half of the 1st dimension. Default: True.
+    - batch_norm (bool): If True, apply batch normalization in every layer. Batch normalization is applied between activation and dropout layer. Default: True.
+    - lr (float): Learning rate. Default: 1e-3.
+    - l2_lambda (float): L2 regularization rate. Default: 1e-3.
+    - criterion (F): Criterion or objective or loss function. Default: F.mse_loss.
+    """
+
+    def __init__(self, field_dims:list, 
+                 embed_dim:int = 10, 
+                 hidden_dims:list = [400, 400, 400], 
+                 activations:Union[str, list]  = "relu", 
+                 dropouts:Union[int, float, list] = 0.5,
+                 batch_norm:bool = True,  
+                 cross_dims:list = [200, 200], 
+                 cross_split_half:bool = True,
+                 lr:float = 1e-3,
+                 weight_decay:float = 1e-3,
+                 criterion:F = F.mse_loss):
+
+        super().__init__(lr, weight_decay, criterion)
+        self.save_hyperparameters()
+
+        if type(dropouts) == float or type(dropouts) == int:
+            dropouts = [dropouts] * len(hidden_dims)
+
+        # Embedding layer
+        self.embedding = FeaturesEmbedding(field_dims, embed_dim)
+
+        # Linear layer
+        self.linear = FeaturesLinear(field_dims)
+
+        # Compressed Interaction
+        self.compressed_interaction = CompressedInteraction(len(field_dims), cross_dims, split_half = cross_split_half)
+
+        # Multilayer Perceptrons
+        self.net = MultilayerPerceptrons(input_dim = len(field_dims) * embed_dim,
+                                         hidden_dims = hidden_dims, 
+                                         activations = activations, 
+                                         dropouts = dropouts,
+                                         output_layer = "relu",
+                                         batch_norm = batch_norm)
+
+    def forward(self, x):
+        # Generate embeddings
+        embed_x = self.embedding(x.int())
+
+        # Prediction
+        y = self.linear(x.int()) + self.compressed_interaction(embed_x) + self.net(torch.flatten(embed_x, start_dim = 1)) 
+
+        return y
