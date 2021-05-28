0270541fa5c208f959b751c8e372e11987aca681
Kyle Ong <ongkyle@live.com.my>
2021-05-28 22:42:30 +0800

    STYLE: update comment and identation
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/model/ncf.py
+++ starreco/model/ncf.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-import numpy as np
+import torch
 import torch.nn.functional as F
 
 from .mf import MF
@@ -14,7 +14,7 @@ class NCF(MF):
     - field_dims (list): List of features dimensions.
     - embed_dim (int): Embedding dimension.
     - hidden_dims (list): List of numbers of neurons throughout the hidden layers. Default: [32,16,8].
-    - activations (str/list): List of activation functions. Default: relu.
+    - activations (str/list): List of activation functions. Default: "relu".
     - dropouts (int/float/list): List of dropout values. Default: 0.5.
     - batch_norm (bool): If True, apply batch normalization in every layer. Batch normalization is applied between activation and dropout layer. Default: True.
     - lr (float): Learning rate. Default: 1e-3.
@@ -35,7 +35,7 @@ class NCF(MF):
         super().__init__(field_dims, embed_dim, lr, l2_lambda, criterion)
         self.save_hyperparameters()
 
-        # Network 
+        # Multilayer Perceptrons layer
         self.net = MultilayerPerceptrons(input_dim = 2 * embed_dim, # strictly only 2
                                          hidden_dims = hidden_dims, 
                                          activations = activations, 
@@ -44,10 +44,11 @@ class NCF(MF):
                                          batch_norm = batch_norm)
 
     def forward(self, x):
-        # Concatenation
-        embed_concat = self.user_item_embeddings(x, concat = True)
-        
-        # Prediction
+        # Concatenate user and item embeddings
+        x_embed = self.features_embedding(x.int())
+        embed_concat = torch.flatten(x_embed, start_dim = 1)
+
+        # Non linear on concatenated user and item embeddings
         y = self.net(embed_concat)
 
         return y
