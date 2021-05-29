a49814991b6d01563582c80455ed6bea73b8e104
Kyle Ong <ongkyle@live.com.my>
2021-05-29 20:14:29 +0800

    FEAT: AutoRec and DeepRec
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

---
+++ starreco/model/autorec.py
@@ -0,0 +1,29 @@
+from .deeprec import DeepRec
+from ..evaluation import masked_mse_loss
+
+# Change mseloss to masked mseloss
+class AutoRec(DeepRec):
+    """
+    AutoRec.
+
+    - input_output_dim (int): Number of neurons in the input and output layer.
+    - hidden_dims (list): List of number of neurons throughout the encoder and decoder (reverse list) hidden layers. Default: [512, 256, 128].
+    - e_activations (str/list): List of activation functions in the encoder layers. Default: "relu".
+    - d_activations (str/list): List of activation functions in the decoder layers. Default: "relu".
+    - lr (float): Learning rate. Default: 1e-3.
+    - l2_lambda (float): L2 regularization rate. Default: 1e-3.
+    - criterion: Criterion or objective or loss function. Default: masked_mse_loss
+    """
+
+    def __init__(self, 
+                 input_output_dim:int,
+                 hidden_dim:int = 256, 
+                 e_activation:str = "relu", 
+                 d_activation:str = "relu", 
+                 dropout:float = 0.5,
+                 batch_norm:bool = True,
+                 lr:float = 1e-3,
+                 weight_decay:float = 1e-3,
+                 criterion = masked_mse_loss):
+        super().__init__(input_output_dim, [hidden_dim], e_activation, d_activation, dropout, batch_norm, lr, weight_decay, criterion)
+        self.save_hyperparameters()
---
+++ starreco/model/deeprec.py
@@ -0,0 +1,51 @@
+from typing import Union
+
+from .module import BaseModule
+from .layers import StackedDenoisingAutoEncoder
+from ..evaluation import masked_mse_loss
+
+# Change mseloss to masked mseloss
+class DeepRec(BaseModule):
+    """
+    DeepRec.
+
+    - input_output_dim (int): Number of neurons in the input and output layer.
+    - hidden_dims (list): List of number of neurons throughout the encoder and decoder (reverse list) hidden layers. Default: [512, 256, 128].
+    - e_activations (str/list): List of activation functions in the encoder layers. Default: "relu".
+    - d_activations (str/list): List of activation functions in the decoder layers. Default: "relu".
+    - lr (float): Learning rate. Default: 1e-3.
+    - l2_lambda (float): L2 regularization rate. Default: 1e-3.
+    - criterion : Criterion or objective or loss function. Default: masked_mse_loss
+    """
+
+    def __init__(self, 
+                 input_output_dim:int,
+                 hidden_dims:list = [512, 256, 128], 
+                 e_activations:Union[str, list] = "relu", 
+                 d_activations:Union[str, list] = "relu", 
+                 dropout:float = 0.5,
+                 batch_norm:bool = True,
+                 lr:float = 1e-3,
+                 weight_decay:float = 1e-3,
+                 criterion = masked_mse_loss):
+        super().__init__(lr, weight_decay, criterion)
+        self.save_hyperparameters()
+
+        self.sdae = StackedDenoisingAutoEncoder(input_output_dim = input_output_dim, 
+                                                hidden_dims = hidden_dims, 
+                                                e_activations = e_activations, 
+                                                d_activations = d_activations,
+                                                e_dropouts = 0,
+                                                d_dropouts = 0,
+                                                dropout = dropout,
+                                                batch_norm = batch_norm,
+                                                extra_input_dim = 0,
+                                                extra_input_all = False,
+                                                noise_factor = 0,
+                                                noise_all = False, 
+                                                mean = 0,
+                                                std = 0)
+
+    def forward(self, x):
+        return self.sdae(x)
+
