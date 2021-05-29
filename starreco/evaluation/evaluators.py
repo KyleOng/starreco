a91482c562e6de104a16d7bc87046de033a31fcf
Kyle Ong <ongkyle@live.com.my>
2021-05-29 22:09:31 +0800

    FEAT: MMSEEvaluator and RMMSEEvaluator
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/evaluation/evaluators.py
+++ starreco/evaluation/evaluators.py
@@ -4,6 +4,8 @@ import torch
 import torch.nn.functional as F
 from tqdm import tqdm
 
+from .metrics import masked_mse_loss
+
 # Done
 class MSEEvaluator:
     """
@@ -29,6 +31,40 @@ class MSEEvaluator:
 class RMSEEvaluator(MSEEvaluator):
     """
     Root Mean Squared Error (RMSE) Evaluator
+
+    - m (model): Recommendation model.
+    - dataloader (list): Data loader.
+    """
+
+    @staticmethod
+    def evaluate(m, dataloader):
+        return math.sqrt(super().evaluate(m, dataloader))
+
+# Done
+class MMSEEvaluator:
+    """
+    Masked Mean Squared Error (MSE) Evaluator
+
+    - m (model): Recommendation model.
+    - dataloader (list): Data loader.
+    """
+
+    @staticmethod
+    def evaluate(m, dataloader):
+        m_infer = m.eval()
+        mmses = torch.tensor([])
+        for batch in tqdm(dataloader):
+            xs = batch[:-1]
+            y = batch[-1]
+            y_hat = m_infer.forward(*xs)
+            mmse = F.masked_mse_loss(y, y_hat).unsqueeze(0)
+            mmses = torch.cat([mmses, mmse])
+        return torch.mean(mmses).item()
+
+# Done
+class RMMSEEvaluator(MMSEEvaluator):
+    """
+    Root Masked Mean Squared Error (RMSE) Evaluator
 
     - m (model): Recommendation model.
     - dataloader (list): Data loader.
