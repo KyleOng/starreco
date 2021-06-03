02bf77de344fc451f1cd5351cad47a944a377b68
Kyle Ong <ongkyle@live.com.my>
2021-06-03 16:11:23 +0800

    FIX: change df_map_column arg argument name to mapper
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/data/utils.py
+++ starreco/data/utils.py
@@ -7,21 +7,20 @@ from torch.utils.data import Dataset, TensorDataset, DataLoader
 from scipy.sparse import coo_matrix, csr_matrix, lil_matrix, vstack, issparse
 
 
-def df_map_column(df:pd.DataFrame, column:str, arg:dict, join:str = "left"):
+def df_map_column(df, column, mapper, join = "left"):
     """
-    Map `df[column]` with according to `arg`.
+    Map `df[column]` with according to `mapper`.
 
-    :param arg (dict): Mapping correspondence.
-
-    :join (str): Type of table join to be performed between `df` and `df_map` 一 a dataframe created from `arg`. Table join will be performed if `df[column].values` do not store the same shape and element as `arg.values()`. Table join will also be disregard if set as None. Default: "left"
+    - mapper (dict): Mapping correspondence.
+    - join (str): Type of table join to be performed between `df` and `df_map` 一 a dataframe created from `mapper`. Table join will be performed if `df[column].values` do not store the same shape and element as `mapper.values()`. Table join will also be disregard if set as None. Default: "left"
     """
     df = df.copy()
-    df[column] = df[column].map(arg)
+    df[column] = df[column].map(mapper)
     if join:
-        # Perform table join if `df[column].values` do not store the same shape and element as `arg.values()`
-        if not np.array_equal(df[column].values, list(arg.values())):
+        # Perform table join if `df[column].values` do not store the same shape and element as `mapper.values()`
+        if not np.array_equal(df[column].values, list(mapper.values())):
             # Create a dataframe from `map`
-            df_map = pd.DataFrame(arg.values(), columns = [column])
+            df_map = pd.DataFrame(mapper.values(), columns = [column])
             # Table join between `df_map` and `df`
             df = df_map.merge(df, on = column, how = join)
     return df.sort_values(column)
@@ -30,16 +29,11 @@ def df_map_column(df:pd.DataFrame, column:str, arg:dict, join:str = "left"):
 def ratings_to_sparse_matrix(users, items, ratings, num_users, num_items):
     """
     Transform user-item ratings dataframe into sparse matrix.
-
-    :param users: List of users.
-
-    :param items: List of items.
-
-    :param ratings: List of ratings.
-
-    :param num_users: number of users.
-
-    :param num_items: number of items.
+    - users: List of users.
+    - items: List of items.
+    - ratings: List of ratings.
+    - num_users: number of users.
+    - num_items: number of items.
 
     :return: sparse rating matrix in csr format.
     """
@@ -56,8 +50,7 @@ def ratings_to_sparse_matrix(users, items, ratings, num_users, num_items):
 def sparse_coo_to_tensor(coo:coo_matrix):
     """
     Transform scipy sparse coo_matrix to pytorch sprase coo_tensor.
-
-    :param coo: scipy sparse coo_matrix.
+    - coo: scipy sparse coo_matrix.
 
     :return: pytorch sparse coo_trensor.
     """
