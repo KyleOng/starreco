849812ed7a67936099ff1853e1ec7ba16c71487b
Kyle Ong <ongkyle@live.com.my>
2021-06-09 16:43:03 +0800

    FIX: Done FGCNN
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/model/fgcnn.py
+++ starreco/model/fgcnn.py
@@ -1,7 +1,6 @@
 import math
-import time
+from typing import Union
 
-import numpy as np
 import torch
 import torch.nn.functional as F
 
@@ -11,7 +10,23 @@ from .layers import FeaturesEmbedding, MultilayerPerceptrons, ActivationFunction
 
 class FGCNN(BaseModule):
     """
-    Feature Generation Convolutional Neural Network.
+    Feature Generation Convolutional Neural Network with Inner Product Neural Network.
+
+    - user_features_split (list): List of integers which determine how user features are splitted. \
+    - item_features_split (list): List of integers which determine how item features are splitted. 
+    - field_dims (list): List of field dimensions.
+    - embed_dim (int): . Default: 40.
+    - rec_filter_size (int): . Default: 3.
+    - cnn_filter_size (list): . Default: [6].
+    - cnn_kernel_size (int): . Default: 100.
+    - cnn_pooling_size (int): . Default: 2.
+    - cnn_activation (str): . Default: "tanh".
+    - net_hidden_dims (list): . Default: [256, 128].
+    - net_activation (str): . Default: "relu".
+    - net_dropout (int/float): . Default: 0.5.
+    - lr (float): Learning rate. Default: 1e-3.
+    - weight_decay (float): L2 regularization rate. Default: 1e-3.
+    - criterion: Criterion or objective or loss function. Default: F.mse_loss.
     """
 
     def __init__(self,
@@ -25,8 +40,8 @@ class FGCNN(BaseModule):
                  cnn_pooling_size:int = 2,
                  cnn_activation:str = "tanh",
                  net_hidden_dims:list = [256, 128], 
-                 net_activations:list = "relu", 
-                 net_dropouts:list = 0.5, 
+                 net_activation:str = "relu", 
+                 net_dropout:list = 0.5, 
                  lr:float = 1e-3,
                  weight_decay:float = 1e-6,
                  criterion = F.mse_loss):
@@ -37,22 +52,30 @@ class FGCNN(BaseModule):
         self.user_features_split = user_features_split
         self.item_features_split = item_features_split
 
+        # Field embedding layer
         self.features_embedding = FeaturesEmbedding(field_dims, embed_dim)
-        self.user_features_embedding = FeaturesEmbedding(user_features_split, embed_dim)
-        self.item_features_embedding = FeaturesEmbedding(item_features_split, embed_dim)
 
+        # User feature embedding layer
+        self.user_features_embedding = torch.nn.ModuleList()
+        for user_feature_split in user_features_split:
+            self.user_features_embedding.append(torch.nn.Linear(user_feature_split, embed_dim, bias = False))
+
+        # Item feature embedding layer
+        self.item_features_embedding = torch.nn.ModuleList()
+        for item_feature_split in item_features_split:
+            self.item_features_embedding.append(torch.nn.Linear(item_feature_split, embed_dim, bias = False))
+
+        # Convolution neural network layers
         self.cnn = torch.nn.ModuleList()
         self.recombinations = torch.nn.ModuleList()
-        new_features_size = 0
-
-        height = v_pad = len(user_features_split) + len(item_features_split)
+        height = v_pad = len(user_features_split) + len(item_features_split) + 2
         width = embed_dim
-        
+        embed_size = height * width
         input_channel_size = 1
         for i in range(len(cnn_filter_sizes)):
             v_pad = int((v_pad - (v_pad - cnn_kernel_size + 1))/ 2)
             
-            # convolution layer
+            # Convolution layer
             output_channel_size = cnn_filter_sizes[i]
             convolution = torch.nn.Conv2d(input_channel_size,
                                           output_channel_size,
@@ -67,16 +90,12 @@ class FGCNN(BaseModule):
                 activation_function = ActivationFunction(cnn_activation)
                 self.cnn.append(activation_function)
 
-            #print(height, width)
-
             # Pooling layer
             pooling = torch.nn.MaxPool2d((cnn_pooling_size, 1))
             self.cnn.append(pooling)
             # Update height and width from pooling layer
             height  = ((height + 2 * pooling.padding - pooling.dilation * (pooling.kernel_size[0] - 1) - 1)/ pooling.stride[0]) + 1
             width = ((width + 2 * pooling.padding - pooling.dilation  * (pooling.kernel_size[1] - 1) - 1)/ pooling.stride[1]) + 1
-
-            #print(height, width)
 
             # Recombination layer
             recombination_input_size = output_channel_size * math.floor(height) * math.floor(width)
@@ -91,92 +110,64 @@ class FGCNN(BaseModule):
             self.recombinations.append(recombination)
 
             # Calculate inner product neural network input size
-            new_features_size += recombination_output_size
+            embed_size += recombination_output_size
+
+            input_channel_size = output_channel_size
 
         # Input size for inner product-based neural network
-        new_embed_size = embed_dim * (len(user_features_split) + len(item_features_split))
-        combine_input_size = new_features_size + new_embed_size
-        inner_product_output_size = (combine_input_size / embed_dim ) * ((combine_input_size / embed_dim) - 1) * embed_dim
-        net_input_size = int(combine_input_size + inner_product_output_size)
+        num_pairs = (embed_size / embed_dim)
+        inner_product_output_size = (num_pairs * (num_pairs - 1) / 2) * embed_dim
+        net_input_size = int(embed_size + inner_product_output_size)
 
         # Inner product-based neural network
-        inner_product = InnerProduct(False)
-        net = MultilayerPerceptrons(input_dim = net_input_size,
+        self.inner_product = InnerProduct(False)
+        self.net = MultilayerPerceptrons(input_dim = net_input_size,
                                     hidden_dims = net_hidden_dims, 
-                                    activations = net_activations, 
-                                    dropouts = net_dropouts,
+                                    activations = net_activation, 
+                                    dropouts = net_dropout,
                                     output_layer = "relu")
-        self.inner_product_net = torch.nn.Sequential(inner_product, net)
 
     def forward(self, x, user_x, item_x):
+        # Field embeddings
+        x_embed = self.features_embedding(x.int())
 
-        batch_size = x.shape[0]
-
+        # User feature embeddings
         user_start = 0
-        user_features_embed = torch.zeros(0).to(self.device)
         for i, user_feature_split in enumerate(self.user_features_split):
-            pad = (i, len(self.user_features_split)-i-1)
             user_end = user_feature_split
             user_end += user_start
-            user_rows, user_feature_indices = (user_x[:, user_start:user_end] == 1).nonzero(as_tuple = True)
-            user_feature_indices_pad = F.pad(input = user_feature_indices.unsqueeze(1), pad = pad, mode = "constant", value = 0)
-            user_feature_embed = self.user_features_embedding(user_feature_indices_pad.int())[:, i]
-            if batch_size != len(user_rows):
-                current_user_row = user_rows[0]
-                user_row_start = 0
-                user_rows = torch.cat((user_rows, torch.tensor([-1])))
-
-                new_user_feature_embed = torch.zeros(0).to(self.device)
-                for i in range(1, len(user_rows)):
-                    next_user_row = user_rows[i]
-                    if current_user_row != next_user_row:
-                        user_row_end = i
-                        user_feature_embed_sum = torch.sum(user_feature_embed[user_row_start: user_row_end, :], dim = 0)
-                        new_user_feature_embed = torch.cat((new_user_feature_embed, user_feature_embed_sum.unsqueeze(0)))
-                        user_row_start = i
-                    current_user_row = next_user_row
-                user_feature_embed = new_user_feature_embed
-            user_features_embed = torch.cat((user_features_embed, user_feature_embed.unsqueeze(1)), dim = 1)
+            user_x_embed = self.user_features_embedding[i](user_x[:, user_start:user_end])
+            x_embed = torch.cat((x_embed, user_x_embed.unsqueeze(1)), dim = 1)
             user_start += user_feature_split
 
+        # Item feature embedding
         item_start = 0
-        item_features_embed = torch.zeros(0).to(self.device)
         for i, item_feature_split in enumerate(self.item_features_split):
-            pad = (i, len(self.item_features_split)-i-1)
             item_end = item_feature_split
             item_end += item_start
-            item_rows, item_feature_indices = (item_x[:, item_start:item_end] == 1).nonzero(as_tuple = True)
-            item_feature_indices_pad = F.pad(input = item_feature_indices.unsqueeze(1), pad = pad, mode = "constant", value = 0)
-            item_feature_embed = self.item_features_embedding(item_feature_indices_pad.int())[:, i]
-            if batch_size != len(item_rows):
-                current_item_row = item_rows[0]
-                item_row_start = 0
-                item_rows = torch.cat((item_rows, torch.tensor([-1]).to(self.device)))
-
-                new_item_feature_embed = torch.zeros(0).to(self.device)
-                for i in range(1, len(item_rows)):
-                    next_item_row = item_rows[i]
-                    if current_item_row != next_item_row:
-                        item_row_end = i
-                        item_feature_embed_sum = torch.sum(item_feature_embed[item_row_start: item_row_end, :], dim = 0)
-                        new_item_feature_embed = torch.cat((new_item_feature_embed, item_feature_embed_sum.unsqueeze(0)))
-                        item_row_start = i
-                    current_item_row = next_item_row
-                item_feature_embed = new_item_feature_embed
-            item_features_embed = torch.cat((item_features_embed, item_feature_embed.unsqueeze(1)), dim = 1)
+            item_x_embed = self.item_features_embedding[i](item_x[:, item_start:item_end])
+            x_embed = torch.cat((x_embed, item_x_embed.unsqueeze(1)), dim = 1)
             item_start += item_feature_split
 
-        x_embed = self.features_embedding(x.int())
-        user_embed, item_embed = x_embed[:, 0], x_embed[:, 1]
-        dot = torch.sum(user_embed * item_embed, dim = 1)
+        # Convolutional neural network feature extraction and recombintation
+        feature = x_embed.unsqueeze(1)
+        i = 0
+        for module in self.cnn:
+            feature = module(feature)
+            if type(module) == torch.nn.MaxPool2d:
+                recombination = self.recombinations[i](feature)
+                recombination = recombination.view(-1, int(recombination.shape[1]/x_embed.shape[-1]), x_embed.shape[-1])
+                x_embed = torch.cat((x_embed, recombination), dim = 1)
+                i += 1
+
+        # Inner product neural network
+        # Inner product on recombined features
+        inner_product = self.inner_product(torch.unbind(x_embed.unsqueeze(2), dim = 1))
+        inner_product = torch.flatten(inner_product, start_dim = 1)
+        x_embed = torch.flatten(x_embed, start_dim = 1)
+        # Concatenate embeddings with inner product output
+        concat = torch.cat((x_embed, inner_product), dim = 1)
+        # Non linear on concatenated features
+        y = self.net(concat)
         
-        # Reshape to match target shape
-        y = dot.view(dot.shape[0], -1)
-
         return y
-
-        #(torch.tensor([0,1,0,1,0]) == 1.).nonzero().flatten().shape[0] == 2
-
-
-
-
