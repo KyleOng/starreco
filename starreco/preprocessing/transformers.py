ce66fd914e9d3c8b1fc067f1479c63e95cc8f361
Kyle Ong <ongkyle@live.com.my>
2021-07-16 14:56:04 +0800

    FIX: fix tqdm for each task
    Signed-off-by: Kyle Ong <ongkyle@live.com.my>

--- starreco/preprocessing/transformers.py
+++ starreco/preprocessing/transformers.py
@@ -107,13 +107,15 @@ class DocTransformer(BaseEstimator, TransformerMixin):
                  max_char:int = 300, 
                  max_df:float = 0.5, 
                  vocab_size:int = 8000,
-                 glove_path:str = "glove.6B/glove.6B.50d.txt"):
+                 glove_path:str = "glove.6B/glove.6B.50d.txt",
+                 progress_bar = True):
         assert max_char > 0 and max_df > 0 and vocab_size > 0
 
         self.max_char = max_char
         self.max_df = max_df
         self.vocab_size = vocab_size
         self.glove_path = glove_path
+        self.progress_bar = progress_bar
 
     def fit(self, X, y = None):
         # Build and reset column transformer for every fit
@@ -122,12 +124,14 @@ class DocTransformer(BaseEstimator, TransformerMixin):
             vectorizer =  TfidfVectorizer(analyzer = "word", 
                                           tokenizer = nltk.word_tokenize,
                                           max_features = None, 
-                                          stop_words = "english") 
+                                          stop_words = "english",) 
             pipe = Pipeline([("imputer", FunctionTransformer(lambda column:column.fillna("").str[:self.max_char])),
                              ("vectorizer", vectorizer)])         
             self.column_transformer.transformers.append((column, pipe, column))
 
         # Calculate TFIDF
+        if self.progress_bar: 
+            print("[Fit] Transforming TFIDF".ljust(38) + ": ...", end='\r')
         tfidf = self.column_transformer.fit_transform(X)
 
         # Get vocabs from X input
@@ -142,27 +146,47 @@ class DocTransformer(BaseEstimator, TransformerMixin):
         # Get associate TFIDF frequency/term/weight for each vocab
         weights = np.ravel(tfidf.mean(axis = 0))
         vocab_weights = {word: weight for word, weight in zip(vocabs_, weights)}
+        if self.progress_bar: 
+            print("[Fit] Transforming TFIDF".ljust(38) + ": 100%")
 
         # Remove corpus specific stop words that have the TFIDF document frequency higher than 0.5 (default).
-        vocab_weights = {word: weight for word, weight in vocab_weights.items() if weight < self.max_df}
+        if self.progress_bar: 
+            vocab_weights_tqdm = tqdm(vocab_weights.items(), total = len(vocab_weights.items()), bar_format = "{desc:}{percentage:3.0f}%|{bar:10}{r_bar}")
+            vocab_weights_tqdm.set_description("[Fit] Removing stopwords".ljust(38))
+        else:
+            vocab_weights_tqdm = vocab_weights.items()
+        vocab_weights = {word: weight for word, weight in vocab_weights_tqdm if weight < self.max_df}
 
         # Load pretrained word embeddings model.
         vocabs = []
         num_lines = sum(1 for _ in open(self.glove_path, 'r', encoding="utf-8"))
         with open(self.glove_path, 'r', encoding="utf-8") as f:
-            f_tqdm = tqdm(f, total = num_lines, bar_format = "{desc:}{percentage:3.0f}%|{bar:10}{r_bar}")
-            f_tqdm.set_description("loading pretrained glove weights (fit)")
+            if self.progress_bar: 
+                f_tqdm = tqdm(f, total = num_lines, bar_format = "{desc:}{percentage:3.0f}%|{bar:10}{r_bar}")
+                f_tqdm.set_description("[Fit] Loading pretrained glove weights".ljust(38))
+            else:
+                f_tqdm = f
             for line in f_tqdm:
                 values = line.split()
                 word = values[0]
                 vocabs.append(word)
                 
         # Keep vocabs which existed in pretrained word embedding vocabularies.
-        vocab_weights = {vocab: weight for vocab, weight in vocab_weights.items() if vocab in vocabs}
+        if self.progress_bar: 
+            vocab_weights_tqdm = tqdm(vocab_weights.items(), total = len(vocab_weights.items()), bar_format = "{desc:}{percentage:3.0f}%|{bar:10}{r_bar}")
+            vocab_weights_tqdm.set_description("[Fit] Keeping words in vocabularies".ljust(38))
+        else:
+            vocab_weights_tqdm = vocab_weights.items()
+        vocab_weights = {vocab: weight for vocab, weight in vocab_weights_tqdm if vocab in vocabs}
 
         # Top 8000 (default) distinct words.
         vocab_weights = dict(sorted(vocab_weights.items(), key = operator.itemgetter(1), reverse = True))
-        vocab_weights = {vocab: vocab_weights[vocab] for vocab in list(vocab_weights.keys())[:self.vocab_size]}
+        if self.progress_bar: 
+            vocabs_tqdm = tqdm(list(vocab_weights.keys())[:self.vocab_size], total = self.vocab_size, bar_format = "{desc:}{percentage:3.0f}%|{bar:10}{r_bar}")
+            vocabs_tqdm.set_description("[Fit] Keeping 8000 distinct words".ljust(38))
+        else:
+            vocabs_tqdm = list(vocab_weights.keys())[:self.vocab_size]
+        vocab_weights = {vocab: vocab_weights[vocab] for vocab in vocabs_tqdm}
 
         # Create vobabulary mapper.
         vocabs = list(vocab_weights.keys())
@@ -184,11 +208,13 @@ class DocTransformer(BaseEstimator, TransformerMixin):
                 non_stops = [token for token in tokens if token.lower() not in stopwords.words('english')]
                 word_indices = [vocab_map[non_stop.lower()] if non_stop.lower() in vocab_map else 1 for non_stop in non_stops]
                 return word_indices
-        
-            tqdm.pandas(desc = f"indexing {x.name} sentences (transform)", 
-                        bar_format = "{desc:}: {percentage:3.0f}%|{bar:10}{r_bar}")
 
-            x_word_indices = x.progress_apply(lambda sentence: sentence_to_indices(sentence))
+            if self.progress_bar: 
+                tqdm.pandas(desc = f"[Transform] Indexing {x.name} sentences".ljust(len(x.name)+38), 
+                            bar_format = "{desc:}: {percentage:3.0f}%|{bar:10}{r_bar}")
+                x_word_indices = x.progress_apply(lambda sentence: sentence_to_indices(sentence))
+            else:
+                x_word_indices = x.apply(lambda sentence: sentence_to_indices(sentence))
 
             return x_word_indices
 
@@ -197,14 +223,17 @@ class DocTransformer(BaseEstimator, TransformerMixin):
             Zero padding word indices until it reached the maximum length.
             For example, [[1,2,3],[1,2,3,4],[1,2]] -> [[1,2,3,0],[1,2,3,4],[1,2,0,0]]
             """
-            tqdm.pandas(desc = f"0padding {x.name} word indices (transform)", 
-                        bar_format = "{desc:}: {percentage:3.0f}%|{bar:10}{r_bar}")
             max_len = x.apply(lambda word_indices: len(word_indices)).max()
 
             # Save transform max len
             self.max_lens_[x.name] = max_len
 
-            x_pad = x.progress_apply(lambda word_indices: np.pad(word_indices, (0, max_len - len(word_indices))))
+            if self.progress_bar: 
+                tqdm.pandas(desc = f"[Transform] Zero padding {x.name} word indices".ljust(len(x.name)+38), 
+                            bar_format = "{desc:}: {percentage:3.0f}%|{bar:10}{r_bar}")
+                x_pad = x.progress_apply(lambda word_indices: np.pad(word_indices, (0, max_len - len(word_indices))))
+            else:
+                x_pad = x.apply(lambda word_indices: np.pad(word_indices, (0, max_len - len(word_indices))))
 
             return x_pad
 
